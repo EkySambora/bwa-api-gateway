82e6c8a18c2d738eb4b8d3d19da807fdc702c6b9
ekysambora <ekysambora@gmail.com>
2023-10-21 06:15:54 +0700

    feat: integrate login & refresh token api

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const { APP_NAME } = process.env
 const usersHandler = require('./handler/users')
 
 router.post("/register", usersHandler.register);
+router.post("/login", usersHandler.login);
 
 module.exports = router;
