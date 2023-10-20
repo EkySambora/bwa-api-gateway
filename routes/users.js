8aadd912ee4f8256b1858f1df7c13dbe510b5cb0
ekysambora <ekysambora@gmail.com>
2023-10-20 08:51:24 +0700

    feat: add api user register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { APP_NAME } = process.env
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource '+ APP_NAME );
-});
+
+const usersHandler = require('./handler/users')
+
+router.post("/register", usersHandler.register);
 
 module.exports = router;
