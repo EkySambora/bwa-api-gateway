ad749fde563b85fbe9b5e18c8713f033d2457278
ekysambora <ekysambora@gmail.com>
2023-10-22 18:42:05 +0700

    feeat: integrate api get user

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,6 @@ const usersHandler = require('./handler/users')
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
 router.put("/", verifyToken, usersHandler.update);
+router.get("/", verifyToken, usersHandler.getUser);
 
 module.exports = router;
