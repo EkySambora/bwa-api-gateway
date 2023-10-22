fc4a831e26ab16929158c200f7cba43cd4f076c9
ekysambora <ekysambora@gmail.com>
2023-10-22 17:20:13 +0700

    feat: create API refresh token

--- routes/handler/refresh-tokens/refreshToken.js
+++ routes/handler/refresh-tokens/refreshToken.js
@@ -14,14 +14,39 @@ module.exports = async(req,res) => {
     try {
         const refreshToken = req.body.refresh_token;
         const email = req.body.email;
+
         if(!refreshToken|| !email){
             return res.status(400).json({
                 status:"error",
                 message: "invalid token"
-            })
+            });
         }
-
+        
         await api.get("/refresh_tokens", { params: { refresh_token: refreshToken} });
+
+        jwt.verify(refreshToken, JWT_SECRET_REFRESH_TOKEN, (err, decode)=>{
+            if(err){
+                return res.status(403).json({
+                    status:"error",
+                    message: err.message
+                })
+            }
+
+            if(email!== decode.data.email){
+                return res.status(400).json({
+                    status:"error",
+                    message: "email is not valid!"
+                })
+            }
+
+            const token = jwt.sign({data: decode.data}, JWT_SECRET, {expiresIn:JWT_ACCESS_TOKEN_EXPIRED});
+            return res.json({
+                status:"success",
+                data:{
+                    token
+                }
+            })
+        })
 
     } catch (error) {
 
