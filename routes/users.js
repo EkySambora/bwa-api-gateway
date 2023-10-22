a92ac390d92eb4d53b60755edf78404cbfd07f82
ekysambora <ekysambora@gmail.com>
2023-10-22 18:31:00 +0700

    feat: integration api update

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { APP_NAME } = process.env
+const verifyToken = require("../middleware/verifyToken");
 
 const usersHandler = require('./handler/users')
 
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
+router.put("/", verifyToken, usersHandler.update);
 
 module.exports = router;
